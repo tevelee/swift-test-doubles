9f3048cb29c14ea7966fd1a068a2bae1a8440cc8
Laszlo Teveli <tevelee@gmail.com>
2026-07-27 14:34:03 +0200

    Gate generic pack fixtures on Catalyst availability

--- Tests/TestDoublesFixtures/UnsupportedGenericLayoutFixtures.swift
+++ Tests/TestDoublesFixtures/UnsupportedGenericLayoutFixtures.swift
@@ -1,9 +1,9 @@
-@available(macOS 14.0, *)
+@available(macOS 14.0, macCatalyst 17.0, *)
 public struct ExternalGenericPack<each Element> {
     public init() {}
 }
 
-@available(macOS 14.0, *)
+@available(macOS 14.0, macCatalyst 17.0, *)
 public struct ExternalConstrainedGenericPack<each Element>
 where repeat each Element: ExternalFirstGenericConstraint {
     public init() {}
